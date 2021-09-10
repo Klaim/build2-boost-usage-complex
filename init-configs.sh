2ba0bd467d6415f55995c25022fa530730cf47f8
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2021-09-10 16:53:24 +0200

    Added comment about bpkg status --all

--- init-configs.sh
+++ init-configs.sh
@@ -21,7 +21,8 @@ b install: build-release/aaa/
 #####
 # NOTES:
 # 1. why do we get libicu? ANSWER: because boost.graph depends on a myriad of libs for no good reason and among them some libs require ICU.
-# 2. `bdep status @common` says nothing is initialized there, which is true. I have to `bdep status @debug -r` to see the dependency tree, but right now it's unreadable.
+# 2. FIXED: `bdep status @common` says nothing is initialized there, which is true. I have to `bdep status @debug -r` to see the dependency tree, but right now it's unreadable.
+#    SOLUTION: build2 now allows `bpkg status --all -d build-config/` which shows the actual dependencies there, not only the initialized projects.
 # 3. FIXED: For that kind of setup (where we want some dependencies to be built once for all and shared), having to specify the specific versions of these special dependencies is a bit problematic for maintenance.
 #    I don't know if there is a way to make bpkg work with the constraints of projects without projects being initialized? Or at least a way to fetch package with a constraint expression instead of a version?
 #    SOLUTION: use `bdep build --dependency --configure-only`, the first flag makes the build command do nothing until the packages are actually used by some other project, the second makes sure we don't build yet.
